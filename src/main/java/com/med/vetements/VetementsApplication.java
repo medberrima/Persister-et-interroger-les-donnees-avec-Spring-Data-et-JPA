e50be7c86b61a057b3baed4fe47747b256a772ee
Mohamed Berrima <medberrima0@gmail.com>
2021-10-23 20:21:25 +0100

    first commit

---
+++ src/main/java/com/med/vetements/VetementsApplication.java
@@ -0,0 +1,13 @@
+package com.med.vetements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetementsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VetementsApplication.class, args);
+  }
+
+}
